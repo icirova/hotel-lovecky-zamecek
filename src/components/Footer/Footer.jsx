67318a4c1e50ea461923b6050034ab19bab88c99
Iveta Círová <cirova.iveta@ptw.cz>
2024-03-11 17:16:14 +0100

    vložení google mapy a adresy

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,18 +6,23 @@ export const Footer = () => {
   <h2>Kontaktujte nás</h2>
   <div className="columns-2">
 
-  <img  className="footer__img" src="/components/Footer/mapa.png" alt="mapa" />
+  
+<div className="footer__img">
+<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d737.526971904469!2d16.101921000000004!3d50.51261100000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTDCsDMwJzQ1LjQiTiAxNsKwMDYnMDYuOSJF!5e1!3m2!1scs!2scz!4v1710173444948!5m2!1scs!2scz" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+ 
+  </div>
+ 
   <div className="address">
     <h3 className="address__title">Adresa:</h3> 
-    <p className="address__item">Hotel u řeky</p>
-    <p className="address__item">Říční 123</p>
-    <p className="address__item">987 65 Říkov</p>
+    <p className="address__item">Lovecký zámeček Brendy</p>
+    <p className="address__item">Panská 101</p>
+    <p className="address__item">542 33 Rtyně v Podkrkonoší</p>
 
     <h3 className="address__title">Telefon:</h3> 
-    <p className="address__item">123 123 123</p>
+    <p className="address__item"> + 420 123 123 123</p>
 
     <h3 className="address__title">E-mail:</h3> 
-    <p className="address__item">info@hotelureky.ab</p>
+    <p className="address__item">info@brendy.ab</p>
   </div>
 </div>
 
